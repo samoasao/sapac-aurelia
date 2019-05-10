15a6296b82011137af45e23442f88259b9132316
samoasao <samoasao@gmail.com>
2019-05-09 20:58:49 -0700

    Homepage

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import {RouterConfiguration, Router} from 'aurelia-router'
+import {PLATFORM} from 'aurelia-framework'
+
 export class App {
-  public message: string = 'Hello World!';
+
+  router: Router;
+
+  configureRouter(config : RouterConfiguration, router: Router){
+    this.router = router;
+    config.title = 'S.A.P.A.C. Home';
+    config.map([
+      { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('home/home.html'), nav: true},
+      { route: 'about', name: 'about', moduleId: PLATFORM.moduleName('about/about.html'), nav: true}
+
+    ]);
+  }
+
 }
