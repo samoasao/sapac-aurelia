80c23bc406396d06f68e96b2377f0a15b7318af0
samoasao <samoasao@gmail.com>
2019-05-16 20:37:36 -0700

    Set up remaining routes

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,11 @@ export class App {
     config.title = 'S.A.P.A.C. Home';
     config.map([
       { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('home/home.html'), nav: true},
-      { route: 'about', name: 'about', moduleId: PLATFORM.moduleName('about/about.html'), nav: true}
-
+      { route: 'about', name: 'about', moduleId: PLATFORM.moduleName('about/about.html'), nav: true},
+      { route: 'auditions', name: 'auditions', moduleId: PLATFORM.moduleName('auditions/auditions.html'), nav: true},
+      { route: 'board', name: 'board', moduleId: PLATFORM.moduleName('board/board.html'), nav: true},
+      { route: 'donate', name: 'donate', moduleId: PLATFORM.moduleName('donate/donate.html'), nav: true},
+      { route: 'joinus', name: 'joinus', moduleId: PLATFORM.moduleName('joinus/joinus.html'), nav: true}
     ]);
   }
 
