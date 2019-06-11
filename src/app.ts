d5f56040bfae29daa55a4f1e79e908d0d229629d
twright <twright@theabr.org>
2019-06-11 11:34:48 -0700

    reorganization and style change

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,12 @@ export class App {
     this.router = router;
     config.title = 'S.A.P.A.C. Home';
     config.map([
-      { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('home/home.html'), nav: true},
-      { route: 'about', name: 'about', moduleId: PLATFORM.moduleName('about/about.html'), nav: true},
-      { route: 'auditions', name: 'auditions', moduleId: PLATFORM.moduleName('auditions/auditions.html'), nav: true},
-      { route: 'board', name: 'about', moduleId: PLATFORM.moduleName('board/board.html'), nav: true},
-      { route: 'donate', name: 'about', moduleId: PLATFORM.moduleName('donate/donate.html'), nav: true},
-      { route: 'joinus', name: 'joinus', moduleId: PLATFORM.moduleName('joinus/joinus.html'), nav: true}
+      { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('pages/home/home.html'), nav: true},
+      { route: 'about', name: 'about', moduleId: PLATFORM.moduleName('pages/about/about.html'), nav: true},
+      { route: 'auditions', name: 'auditions', moduleId: PLATFORM.moduleName('pages/auditions/auditions.html'), nav: true},
+      { route: 'board', name: 'about', moduleId: PLATFORM.moduleName('pages/board/board.html'), nav: true},
+      { route: 'donate', name: 'about', moduleId: PLATFORM.moduleName('pages/donate/donate.html'), nav: true},
+      { route: 'joinus', name: 'joinus', moduleId: PLATFORM.moduleName('pages/joinus/joinus.html'), nav: true}
     ]);
   }
 
